dff1c369107b16a2c718b0305241f10426d09d3c
Kay Mok <kaymok3@gmail.com>
2018-01-26 12:40:12 -0500

    Create bundle

--- gulpfile.js
+++ gulpfile.js
@@ -33,3 +33,29 @@ var notify = function (error) {
 
     notifier.notify({title: title, message: message});
 };
+
+// Bundle settings
+var bundler = watchify(browserify({
+    entries: ['./src/app.jsx'],
+    transform: [reactify],
+    extensions: ['.jsx'],
+    debug: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+}));
+
+// Bundle tasks
+function bundle() {
+    return bundler
+        .bundle()
+        .on('error', notify)
+        .pipe(source('main.js'))
+        .pipe(gulp.dest('./'))
+}
+bundler.on('update', bundle);
+
+// Create bundle
+gulp.task('build', function() {
+    bundle()
+});
