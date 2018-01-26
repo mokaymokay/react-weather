ae63aabe7a0da68ba718136e2aceb5456c1107ed
Kay Mok <kaymok3@gmail.com>
2018-01-26 12:32:28 -0500

    Require dependencies and format error messages

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var notifier = require('node-notifier');
+var server = require('gulp-server-livereload');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+// Format error messages
+var notify = function (error) {
+    var message = 'In: ';
+    var title = 'Error: ';
+
+    if (error.description) {
+        title += error.description;
+    }
+    else if (error.message) {
+        title += error.message;
+    }
+
+    if (error.filename) {
+        var file = error.filename.split('/');
+        message += file[file.length-1];
+    }
+
+    if (error.lineNumber) {
+        message += '\nOn Line: ' + error.lineNumber;
+    }
+
+    notifier.notify({title: title, message: message});
+};
